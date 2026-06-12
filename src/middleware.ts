59006a6965a90bd55282594984ea13ec54597390
Mr. Robot <tb.vandiza13@gmail.com>
2026-06-12 13:57:31 +0700

    fix: nextauth middleware routing and decimals serialization

--- src/middleware.ts
+++ src/middleware.ts
@@ -61,6 +61,6 @@ export default auth((req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|json|txt|js)).*)",
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|json|txt|js)).*)",
   ],
 };
